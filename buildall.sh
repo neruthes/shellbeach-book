20e07904985822a6d0929806439c2817c30fb836
Neruthes <i@neruthes.xyz>
2022-03-06 11:00:22 +0000

    Regular update '2022-03-06T11:00:22+00:00'

--- buildall.sh
+++ buildall.sh
@@ -2,13 +2,23 @@
 
 REPODIR=$PWD
 
-YEARS="2022"
-for i in $YEARS; do
-    cd $i
+
+function _buildYear() {
+    cd $YEAR
     ./build.sh
+    if [[ "$@" != "" ]]; then
+        ### If any argument is given, make a clean prebuild before main build
+        ./build.sh
+    fi
     ./build.sh $@
+    cd $REPODIR
+}
+
+
+YEARS="2022"
+for YEAR in $YEARS; do
+    _buildYear $YEAR
 done
 
 
-cd $REPODIR
 pushgithubdistweb
